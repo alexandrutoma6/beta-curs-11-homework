71ee2865869ca350be65a0d1f4ba96da7e982895
Toma Alexandru <alextoma73.at@gmail.com>
2024-07-20 08:10:08 +0300

    Move the logic from ex2 Main to a new PersonManager class

--- src/org/beta/curs11/homework/ex2/Main.java
+++ src/org/beta/curs11/homework/ex2/Main.java
@@ -1,5 +1,6 @@
 package org.beta.curs11.homework.ex2;
 
+import org.beta.curs11.homework.ex2.managers.PersonManager;
 import org.beta.curs11.homework.ex2.models.Person;
 
 import java.util.ArrayList;
@@ -16,65 +17,12 @@ public class Main {
                 new Person("Sebastian", 40, "Blonde"),
                 new Person("Eva", 35, "Red"));
 
-        System.out.println(getNameList(persons));
-        System.out.println(getNameToAge(persons));
-        System.out.println(getPersonsOlderThan(30, persons));
-        System.out.println(getHairColorToName(persons));
-        System.out.println(getAgeToName(persons));
-    }
+        PersonManager personManager = new PersonManager();
 
-    private static List<String> getNameList(List<Person> persons) {
-        List<String> result = new ArrayList<>();
-        for (Person person : persons) {
-            result.add(person.name());
-        }
-        return result;
-    }
-
-    private static Map<String, Integer> getNameToAge(List<Person> persons) {
-        Map<String, Integer> result = new HashMap<>();
-        for (Person person : persons) {
-            result.put(person.name(), person.age());
-        }
-        return result;
-    }
-
-    private static List<Person> getPersonsOlderThan(Integer givenAge, List<Person> persons) {
-        List<Person> olderThanGivenAge = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.age() > givenAge) {
-                olderThanGivenAge.add(person);
-            }
-        }
-        return olderThanGivenAge;
-    }
-
-    private static Map<String, List<String>> getHairColorToName(List<Person> persons) {
-        Map<String, List<String>> result = new HashMap<>();
-
-        for (Person person : persons) {
-            String hairColour = person.hairColour();
-            String name = person.name();
-
-            if (!result.containsKey(hairColour)) {
-                result.put(hairColour, new ArrayList<>());
-            }
-            result.get(hairColour).add(name);
-        }
-        return result;
-    }
-
-    private static Map<Integer, List<Person>> getAgeToName(List<Person> persons) {
-        Map<Integer, List<Person>> result = new HashMap<>();
-
-        for (Person person : persons) {
-            int age = person.age();
-
-            if (!result.containsKey(age)) {
-                result.put(age, new ArrayList<>());
-            }
-            result.get(age).add(person);
-        }
-        return result;
+        System.out.println(personManager.getNameList(persons));
+        System.out.println(personManager.getNameToAge(persons));
+        System.out.println(personManager.getPersonsOlderThan(30, persons));
+        System.out.println(personManager.getHairColorToName(persons));
+        System.out.println(personManager.getAgeToName(persons));
     }
 }
---
+++ src/org/beta/curs11/homework/ex2/managers/PersonManager.java
@@ -0,0 +1,66 @@
+package org.beta.curs11.homework.ex2.managers;
+
+import org.beta.curs11.homework.ex2.models.Person;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PersonManager {
+
+    public List<String> getNameList(List<Person> persons) {
+        List<String> result = new ArrayList<>();
+        for (Person person : persons) {
+            result.add(person.name());
+        }
+        return result;
+    }
+
+    public Map<String, Integer> getNameToAge(List<Person> persons) {
+        Map<String, Integer> result = new HashMap<>();
+        for (Person person : persons) {
+            result.put(person.name(), person.age());
+        }
+        return result;
+    }
+
+    public List<Person> getPersonsOlderThan(Integer givenAge, List<Person> persons) {
+        List<Person> olderThanGivenAge = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.age() > givenAge) {
+                olderThanGivenAge.add(person);
+            }
+        }
+        return olderThanGivenAge;
+    }
+
+    public Map<String, List<String>> getHairColorToName(List<Person> persons) {
+        Map<String, List<String>> result = new HashMap<>();
+
+        for (Person person : persons) {
+            String hairColour = person.hairColour();
+            String name = person.name();
+
+            if (!result.containsKey(hairColour)) {
+                result.put(hairColour, new ArrayList<>());
+            }
+            result.get(hairColour).add(name);
+        }
+        return result;
+    }
+
+    public  Map<Integer, List<Person>> getAgeToName(List<Person> persons) {
+        Map<Integer, List<Person>> result = new HashMap<>();
+
+        for (Person person : persons) {
+            int age = person.age();
+
+            if (!result.containsKey(age)) {
+                result.put(age, new ArrayList<>());
+            }
+            result.get(age).add(person);
+        }
+        return result;
+    }
+}
